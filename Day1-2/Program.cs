858be898ea88be3c383152092afc5f25ed08360b
MrOnosa <supersnapple@gmail.com>
2021-12-02 05:34:38 -0600

    Day 1-2

---
+++ Day1-2/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int increase = 0;
+            var values = new List<int>();
+            var slidingWindowA = new Queue<int>();
+            var slidingWindowB = new Queue<int>();
+  
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@".\input.txt"))
+            {  
+                var thisValue = int.Parse(line);
+                values.Add(thisValue);
+            }
+            //Seed
+            slidingWindowA.Enqueue(values[0]);
+            slidingWindowA.Enqueue(values[1]);
+            slidingWindowB.Enqueue(values[1]);
+            slidingWindowB.Enqueue(values[2]);
+
+
+            for (int i = 3; i < values.Count; i++)
+            {
+                slidingWindowA.Enqueue(values[i-1]);
+                slidingWindowB.Enqueue(values[i]);
+                if(slidingWindowA.Count > 3){
+                    slidingWindowA.Dequeue();
+                }
+                if(slidingWindowB.Count > 3){
+                    slidingWindowB.Dequeue();
+                }
+
+                var sumA = slidingWindowA.Sum();
+                var sumB = slidingWindowB.Sum();
+                var listA = slidingWindowA.ToList();
+                var listB = slidingWindowB.ToList();
+                System.Console.WriteLine($"{i}: SumA: {listA[0]}, {listA[1]}, {listA[2]}: {sumA}."); 
+                System.Console.WriteLine($"{i}: SumB: {listB[0]}, {listB[1]}, {listB[2]}: {sumB}."); 
+                if(sumB > sumA){
+                    increase++;                            
+                    System.Console.WriteLine("Increase detected! That's {0}.", increase); 
+                }               
+                
+            }
+            
+            System.Console.WriteLine("There were {0} increases.", increase); 
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # AdventOfCode2021
+
+This is really hacky code to get these tasks done. They are not indicative of my typical programming style.
+
+New folder with template DayX-Y
+dotnet new console --framework net6.0
+dotnet new gitignore
