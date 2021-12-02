8294e440d24cc67d81e617f0d8fb5653af669fef
MrOnosa <supersnapple@gmail.com>
2021-12-01 21:08:24 -0600

    First puzzle completed.

---
+++ Day1-1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int counter = 0;  
+            int increase = 0;
+            int lastValue = int.MinValue;
+            bool first = true;
+  
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@".\input.txt"))
+            {  
+                System.Console.WriteLine(line);  
+                counter++;  
+                var thisValue = int.Parse(line);
+                if(!first){
+                    if(thisValue > lastValue){
+                        increase++;
+                        System.Console.WriteLine("Increase detected! That's {0}.", increase); 
+                    }
+
+                }
+                lastValue = thisValue;
+                first = false;
+            }  
+            
+            System.Console.WriteLine("There were {0} lines.", counter);  
+            System.Console.WriteLine("There were {0} increases.", increase); 
+            // Suspend the screen.  
+            System.Console.ReadLine();
+        }
+    }
+}
