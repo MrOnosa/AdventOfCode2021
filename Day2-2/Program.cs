c68db5431a60480317926c774ab8f719e2109449
MrOnosa <supersnapple@gmail.com>
2021-12-02 18:40:18 -0600

    Day 2-2

---
+++ Day2-2/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int horizontalPosition = 0;
+            int depth = 0;
+            int aim = 0;
+            var values = new List<Step>();
+  
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@".\input.txt"))
+            {  
+                var direction = line.Split()[0];
+                var thisValue = int.Parse(line.Split()[1]);
+                values.Add(new Step{Direction = direction, Delta = thisValue});
+            }
+
+            for (int i = 0; i < values.Count; i++)
+            {          
+                var value = values[i];
+                System.Console.WriteLine("Step: {0} {1}", value.Direction, value.Delta); 
+                if(value.Direction == "forward"){
+                    horizontalPosition += value.Delta;
+                    depth += aim * value.Delta;
+                } else if(value.Direction == "down"){
+                    aim += value.Delta;
+                } else {
+                    aim -= value.Delta;
+                }
+                System.Console.WriteLine("Heading: {0} {1}", horizontalPosition, depth); 
+            }
+            
+            System.Console.WriteLine("Result {0}", horizontalPosition * depth); 
+        }
+    }
+
+    class Step
+    {
+        public string Direction {get;set;}
+        public int Delta {get;set;}
+    }
+}
--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 This is really hacky code to get these tasks done. They are not indicative of my typical programming style.
 
 New folder with template DayX-Y  
-dotnet new console --framework net6.0  
-dotnet new gitignore
+`dotnet new console --framework net6.0`
+`dotnet new gitignore`
