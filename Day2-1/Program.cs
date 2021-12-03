90567545bf5e1f68c612b5a0988f288170a4c721
MrOnosa <supersnapple@gmail.com>
2021-12-02 18:32:34 -0600

    Day 2-1

---
+++ Day2-1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int horizontalPosition = 0;
+            int depth = 0;
+            var values = new List<Step>();
+  
+            // Read the file and display it line by line.  
+            foreach (string line in System.IO.File.ReadLines(@".\input.txt"))
+            {  
+                var direction = line.Split()[0];
+                var thisValue = int.Parse(line.Split()[1]);
+                values.Add(new Step{Direction = direction, Delta = thisValue});
+            }
+
+            for (int i = 0; i < values.Count; i++)
+            {          
+                var value = values[i];
+                System.Console.WriteLine("Step: {0} {1}", value.Direction, value.Delta); 
+                if(value.Direction == "forward"){
+                    horizontalPosition += value.Delta;
+                } else if(value.Direction == "down"){
+                    depth += value.Delta;
+                } else {
+                    depth -= value.Delta;
+                }
+                System.Console.WriteLine("Heading: {0} {1}", horizontalPosition, depth); 
+            }
+            
+            System.Console.WriteLine("Result {0}", horizontalPosition * depth); 
+        }
+    }
+
+    class Step
+    {
+        public string Direction {get;set;}
+        public int Delta {get;set;}
+    }
+}
