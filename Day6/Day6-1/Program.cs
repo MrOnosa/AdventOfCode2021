840688d75c97eca70175c7d127ba33415d58eac2
MrOnosa <supersnapple@gmail.com>
2021-12-06 14:35:32 -0600

    Day 6

---
+++ Day6/Day6-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var lanternfishes = new List<int>();
+            foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\test-input.txt"))
+            {
+                Console.WriteLine($"Input: {line}");
+                foreach (var number in line.Split(','))
+                {
+                    lanternfishes.Add(int.Parse(number));
+                }
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            Console.WriteLine($"Inital State: {string.Join(',', lanternfishes)}");
+            stopWatch.Start();
+            for (int day = 0; day < 80; day++)
+            {
+                var agedFish = new List<int>();
+                int newFish = 0;
+                foreach (var lanternfish in lanternfishes)
+                {
+                    if(lanternfish == 0){
+                        newFish++;
+                        agedFish.Add(6);
+                    } else {
+                        agedFish.Add(lanternfish - 1);
+                    }
+                }
+
+                lanternfishes = agedFish;
+                for (int i = 0; i < newFish; i++)
+                {
+                    lanternfishes.Add(8);
+                }
+                Console.WriteLine($"After {day:00} Days: {stopWatch.Elapsed}");
+                //Console.WriteLine($"After {day:00} Days: {string.Join(',', lanternfishes)}");
+            }
+            stopWatch.Stop();
+            //Console.WriteLine($"After 80 Days: {string.Join(',', lanternfishes)}");
+
+            Console.WriteLine($"Total laternfish: {lanternfishes.Count}");
+
+        }
+    }
+}
---
+++ Day6/Day6-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            long[] buckets = new long[9];
+            foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\puzzle-input.txt"))
+            {
+                Console.WriteLine($"Input: {line}");
+                foreach (var number in line.Split(','))
+                {
+                    buckets[int.Parse(number)]++;
+                }
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            for (int day = 0; day < 256; day++)
+            {
+                long newFish = buckets[0];
+                buckets[0] = buckets[1];
+                buckets[1] = buckets[2];
+                buckets[2] = buckets[3];
+                buckets[3] = buckets[4];
+                buckets[4] = buckets[5];
+                buckets[5] = buckets[6];
+                buckets[6] = buckets[7] + newFish;
+                buckets[7] = buckets[8];
+                buckets[8] = newFish;
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine($"Total laternfish: {buckets.Sum()} - Elapsed {stopWatch.Elapsed} ");
+
+        }
+    }
+}
