c2b89708ad11d6511327fb9617372ee8999eb339
MrOnosa <supersnapple@gmail.com>
2021-12-07 12:15:42 -0600

    Day 7

---
+++ Day7/Day7-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+            Console.WriteLine("Hello World!");
+            List<int> crabs = new List<int>();
+            foreach (string line /*Store text into string records*/ in System.IO.File.ReadLines(@".\..\puzzle-input.txt"))
+            {
+                Console.WriteLine($"Input: {line}");
+                foreach (var number in line.Split(','))
+                {
+                    crabs.Add(int.Parse(number));
+                }
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int bestPosition = -1;
+            int bestFuelCost = int.MaxValue;
+            int minHorizontalPosition = crabs.Min();
+            int maxHorizontalPosition = crabs.Max();
+            for (int i = minHorizontalPosition; i <= maxHorizontalPosition; i++)
+            {
+                int fuelCost = 0;
+                foreach (var crab in crabs)
+                {
+                    if(crab < i){
+                        fuelCost += i - crab;
+                    } else {
+                        fuelCost += crab - i;
+                    }
+                }
+                Console.WriteLine($"Position {i} - Cost {fuelCost} - Elapsed {stopWatch.Elapsed} ");        
+                if(fuelCost < bestFuelCost){
+                    bestFuelCost = fuelCost;
+                    bestPosition = i;
+                }
+            }
+            stopWatch.Stop();
+
+            Console.WriteLine($"Total fuel cost: {bestFuelCost} at position {bestPosition} - Elapsed {stopWatch.Elapsed} ");
